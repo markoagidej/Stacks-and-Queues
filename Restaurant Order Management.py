d84168d519096b1587d1ef2e27bc2f3667bb68a3
Marko <markogidej@yahoo.com>
2024-08-06 16:36:45 -0400

    Task 1 and 2 done

--- Restaurant Order Management.py
+++ Restaurant Order Management.py
@@ -12,6 +12,9 @@ class kitchenOrderStack:
     def pop(self):
         self.orders.pop(0)
 
+    def view(self):
+        print(self.orders)
+
 # Task 3 Design a queue-based data structure to represent the customer order queue, where orders are prioritized based on factors such as customer waiting time and order complexity.
 
 
@@ -22,3 +25,9 @@ class kitchenOrderStack:
 
 # Task 5 Test the order management system with sample orders to ensure its correctness and efficiency.
 
+kitchenOrders = kitchenOrderStack()
+kitchenOrders.push("bread")
+kitchenOrders.push("water")
+kitchenOrders.view()
+kitchenOrders.pop()
+kitchenOrders.view()
